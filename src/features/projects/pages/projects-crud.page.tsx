cfba26108604b37ded8dec1cf45754aff046ed38
i4RenAlejo <jgrisales@i4digital.com>
2023-11-09 08:13:17 -0500

    Corrección bug #9578 validación de presupuesto

--- src/features/projects/pages/projects-crud.page.tsx
+++ src/features/projects/pages/projects-crud.page.tsx
@@ -32,9 +32,10 @@ function ProjectsCrudPage(): React.JSX.Element {
         url: formAction == "edit" ? "/direccion-estrategica/proyectos/edit" : "/direccion-estrategica/proyectos/crear-proyecto",
         extraParams: formAction == "edit" ? `/${idProyect}` : undefined,
     });
+    console.log('textContinue:', textContinue );
     const statusValidation = projectData?.status == 2 || projectData?.status == 3;
     const textBtnUpdateStatus = statusValidation ? "Actualizar estado" : "Guardar temporalmente" ;
-    const btnContinueDisableValidation = disableContinue || ( statusValidation && tabs[step]?.id != 'transfer' && textContinue != "Guardar y regresar" && textContinue != "Aceptar");
+    const btnContinueDisableValidation = disableContinue || ( statusValidation && tabs[step]?.id != 'transfer' && !textContinue );
     if (!projectData?.status && formAction === "edit") { return <p>Cargando...</p>; }
     return (
         <div className='crud-page full-height'>
