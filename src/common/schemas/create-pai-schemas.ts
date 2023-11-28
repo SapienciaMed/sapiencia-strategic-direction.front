5aaa065bf9acace2531f24cb2622365ca73bb9f6
davidhh98 <136001294+dhenao-i4digital@users.noreply.github.com>
2023-11-28 15:41:10 -0500

    ajuste bugs

--- src/common/schemas/create-pai-schemas.ts
+++ src/common/schemas/create-pai-schemas.ts
@@ -1,11 +1,11 @@
 import * as yup from "yup";
 
 export const CreatePAIValidator = yup.object({
-    yearPAI: yup.number().required("Debe seleccionar una opcion"),
-    budgetPAI: yup.number().required("Debe seleccionar una opcion"),
-    typePAI: yup.number().required("Debe seleccionar una opcion"),
-    namePAI: yup.number().required("Debe seleccionar una opcion"),
+    yearPAI: yup.number().required("Debe seleccionar una opción"),
+    budgetPAI: yup.number().required("Debe seleccionar una opción"),
+    typePAI: yup.number().required("Debe seleccionar una opción"),
+    namePAI: yup.number().required("Debe seleccionar una opción"),
     objectivePAI: yup.string().required("El campo es obligatorio"),
     articulationPAI: yup.string().required("El campo es obligatorio").max(200, "Solo se permiten 200 caracteres"),
-    selectedRisk: yup.number().required("Debe seleccionar una opcion"),
+    selectedRisk: yup.number().required("Debe seleccionar una opción"),
 });
