2124be902f6ae2c3637a273e61effb2233c1773e
FrankAsgard <ramslipfrank@gmail.com>
2023-12-26 14:59:35 -0500

    HU 69

--- src/features/anticorruption-plan/components/anticorruption-table-expansible.component.tsx
+++ src/features/anticorruption-plan/components/anticorruption-table-expansible.component.tsx
@@ -1,4 +1,9 @@
+import { Tooltip } from "primereact/tooltip";
 import React from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { PiTrash } from "react-icons/pi";
+import { Navigate, useNavigate } from "react-router-dom";
+import "../style/formulation.scss"; 
 
 interface IAntiCorruptionExpansibleTableProps {
     index: any;
@@ -6,17 +11,37 @@ interface IAntiCorruptionExpansibleTableProps {
 }
 
 const AntiCorruptionExpansibleTable = ({ index, onDelete }: IAntiCorruptionExpansibleTableProps) => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/direccion-estrategica/planes/plan-anticorrupcion/formular-plan');
+    };
     return (
         <tr>
-            <td>No. Componente {index}</td>
-            <td>
-                <input type="text" placeholder="Descripción de componente" />
+            <td className="p-datatable-PAA">No. Componente {index}</td>
+            <td className="p-datatable-PAA">
+                <input type="text" className="input-PAA" placeholder="Escribe aquí" />
             </td>
-            <td>
-                <button onClick={onDelete}>
-                    {/* Icono de eliminación */}
-                    Delete
-                </button>
+            <td className="p-datatable-PAA">
+                <div className="icon-content">
+                    <><Tooltip target=".create-action" /><div
+                        className="create-action"
+                        data-pr-tooltip="Agregar actividad"
+                        data-pr-position="bottom"
+                        style={{ 'color': '#D72FD1', cursor: 'pointer' }}
+                        onClick={handleClick}
+                    >
+                        <AiOutlinePlusCircle />
+                    </div></>
+                    <><Tooltip target=".delete-action" /><div
+                        className="delete-action"
+                        data-pr-tooltip="Eliminar Acción"
+                        data-pr-position="bottom"
+                        style={{ 'color': '#e53935' }}
+                        onClick={onDelete}
+                    >
+                        <PiTrash className="button grid-button button-delete" />
+                    </div></>
+                </div>
             </td>
         </tr>
     );
--- src/features/projects/pages/formulation-PAAC.tsx
+++ src/features/projects/pages/formulation-PAAC.tsx
@@ -1,14 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
 import AntiCorruptionExpansibleTable from "../../anticorruption-plan/components/anticorruption-table-expansible.component";
 import { AppContext } from "../../../common/contexts/app.context";
-import { SelectComponent } from "../../../common/components/Form";
+import { ButtonComponent, SelectComponent } from "../../../common/components/Form";
 import { useAntiCorruptionPlanData } from "../../anticorruption-plan/hooks/anti-corruption-plan.hook";
+import "../../anticorruption-plan/style/formulation.scss";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const FormulationPAAC = () => {
     const { navigate,
-            control, 
-            statusData, 
-            errors } = useAntiCorruptionPlanData();
+        control,
+        statusData,
+        errors } = useAntiCorruptionPlanData();
     const [components, setComponents] = useState([]);
     const [componentCount, setComponentCount] = useState(1);
     const [deleteConfirmation, setDeleteConfirmation] = useState({});
@@ -49,12 +52,12 @@ const FormulationPAAC = () => {
                         setMessage({});
                     },
                 });
-    
+
                 deleteRow(idToDelete); // Llamada para eliminar el componente
             },
         });
     };
-    
+
 
     const deleteRow = (idToDelete) => {
         setComponents((prevComponents) =>
@@ -70,46 +73,78 @@ const FormulationPAAC = () => {
         }
     }, [components]);
 
+    const Navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/direccion-estrategica/planes/plan-anticorrupcion');
+    };
+
     return (
-        <div>
-            <h2>Formular Plan Anticorrupción y Atención al Ciudadano (PAAC)</h2>
-            <section>
-                <SelectComponent
-                    control={control}
-                    idInput={"status"}
-                    className={`select-basic span-width`}
-                    label="Año"
-                    classNameLabel="text-black biggest bold"
-                    data={statusData}
-                    errors={errors}
-                    filter={true}
-                />
-            </section>
-            <section>
-                <h3>Componentes</h3>
-                <button onClick={handleAddComponent}>
-                    Agregar componente
-                </button>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>No. Componente</th>
-                            <th>Descripción de componente</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {components.map((component) => (
-                            <AntiCorruptionExpansibleTable
-                                key={component.id}
-                                index={component.index}
-                                onDelete={() => handleDeleteComponent(component.id)}
+        <>
+            <div className="main-page">
+                <div className="card-table">
+                    <h2>Formular Plan Anticorrupción y Atención al Ciudadano (PAAC)</h2>
+                    <div className="card-table">
+                        <section className="select_PAAC">
+                            <SelectComponent
+                                control={control}
+                                idInput={"status"}
+                                className={`select-basic span-width`}
+                                label="Año"
+                                classNameLabel="text-black biggest bold"
+                                data={statusData}
+                                errors={errors}
+                                filter={true}
                             />
-                        ))}
-                    </tbody>
-                </table>
-            </section>
-        </div>
+                        </section>
+                    </div>
+                    <section className="card-table mt-15">
+                        <h3>Componentes</h3>
+                        <div className="text-buttom-circle" onClick={handleAddComponent}>
+                            Agregar componente <AiOutlinePlusCircle />
+                        </div>
+                        <div className="card-table mt-15">
+                            <table className="table-PAA">
+                                <thead>
+                                    <tr>
+                                        <th className="p-datatable-thead-PAA">No. Componente</th>
+                                        <th className="p-datatable-thead-PAA">Descripción de componente</th>
+                                        <th className="p-datatable-thead-PAA">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {components.map((component) => (
+                                        <AntiCorruptionExpansibleTable
+                                            key={component.id}
+                                            index={component.index}
+                                            onDelete={() => handleDeleteComponent(component.id)}
+                                        />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+                    <div className="container-button-bot space-between">
+                        <div>
+                        <ButtonComponent
+                                className={`button-main huge hover-three button-save`}
+                                value={"Guardar temporalmente"}
+                                type="button"
+                            />
+                        </div>
+                        <div className="buttons-bot">
+                            <span className="bold text-center button" onClick={handleClick}>
+                                Cancelar
+                            </span>
+                            <ButtonComponent
+                                className={`button-main huge hover-three button-save`}
+                                value={"Guardar y regresar"}
+                                type="button"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
